9fbfef19f9a38b4ea6a644c90ee6a05ab3f57469
zzzy <zhiyuan.zhang@adelaide.edu.au>
2023-03-28 14:28:40 +1100

    update doc

--- bunnyhop_evict/compile_kernel.bash
+++ bunnyhop_evict/compile_kernel.bash
@@ -4,7 +4,9 @@ rm -f linux-*.deb
 rm -f linux-5.12.4_1.colorKernel*
 rm -rf kernel/
 cp -r linux/ kernel/
-cp .config kernel/
+#cp .config kernel/
+cp /usr/src/linux-headers-$(uname -r)/.config kernel/ ### Or you could generate it with make menuconfig
+
 
 cd kernel/
 #scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
